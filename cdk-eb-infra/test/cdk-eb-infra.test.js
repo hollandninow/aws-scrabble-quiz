6d6c1ab205ca2d3c2a37e7db141a83ab77ab6308
Holland Ninow <holland.ninow@gmail.com>
2023-03-04 15:31:28 -0800

    aws cdk init

---
+++ cdk-eb-infra/test/cdk-eb-infra.test.js
@@ -0,0 +1,18 @@
+// const { Template } = require('@aws-cdk/assertions');
+// const cdk = require('@aws-cdk/core');
+// const CdkEbInfra = require('../lib/cdk-eb-infra-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-eb-infra-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new CdkEbInfra.CdkEbInfraStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
+
